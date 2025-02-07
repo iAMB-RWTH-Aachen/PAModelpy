6b31c0fa8776d1cdaeafa1b8d28620b0b5105ae2
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-02-07 09:06:52 +0100

    updated requirements to avoid conflict with pandas

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PAMpy',
+    name='PAModelpy',
     version='1.0',
     packages=find_packages(),
     install_requires=[
         'PAModelpy',
-        'plotly==5.13.0',
         'matplotlib==0.1.6',
         'scipy',
         'time',
