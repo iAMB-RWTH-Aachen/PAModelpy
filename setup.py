a92962ef2635f6e7d943b622dc851bc3e9e9361a
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-02-20 14:43:11 +0100

    python 3.12 gives issues with setuptools, thus documented that the python version should be between 3.9 and 3.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PAModelpy',
-    version='1.0',
+    version='0.0.4.7',
     packages=find_packages(),
     install_requires=[
         'PAModelpy',
