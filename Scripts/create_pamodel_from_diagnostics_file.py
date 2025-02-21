13b7f23c114fcae2976fa0cb93ea6663022171d9
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2025-02-21 16:48:20 +0100

    minor adjustments

---
+++ Scripts/create_pamodel_from_diagnostics_file.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from typing import Tuple, Literal
+import re
+
+from src.PAModelpy.PAModel import PAModel
+from src.PAModelpy.utils.pam_generation import set_up_pam, parse_reaction2protein, _order_enzyme_complex_id
+
+DEFAULT_MOLMASS = 39959.4825 #kDa
+DEFAULT_KCAT = 11 #s-1
+
+def create_pamodel_from_diagnostics_file(file_path:str, model: PAModel)-> PAModel:
+    best_individual_df = pd.read_excel(file_path, sheet_name='Best_Individuals')
+    for _, group in best_individual_df.groupby('run_id'):
+        for _, row in group.iterrows():
+            rxn_id = _extract_reaction_id_from_catalytic_reaction_id(row['rxn_id'])
+            enzyme_id = _order_enzyme_complex_id(row['enzyme_id'])
+            kcat_dict = {rxn_id: {row['direction']: row['kcat[s-1]']}}
+            model.change_kcat_value(enzyme_id=enzyme_id, kcats=kcat_dict)
+    return model
+
+def get_rxn2kcat_protein2gene_dict(param_file_path:str, model_file_path: str
+                                   ) -> Tuple[
+    dict[str, dict[str,dict[Literal['f', 'b', 'molmass', 'protein_reaction_association'], float]]],
+    dict[str,str]]:
+    # create enzyme objects for each gene-associated reaction
+    pam = set_up_pam(param_file_path, model_file_path, sensitivity=False)
+    enzyme_db = pd.read_excel(param_file_path, sheet_name='ActiveEnzymes').iloc[:, 1:]
+    rxn2protein, protein2gene = parse_reaction2protein(enzyme_db, pam)
+
+    ae_sector = pam.sectors.ActiveEnzymeSector
+    new_rxn2prot = ae_sector.rxn2protein.copy()
+    for rxn, enz_dict in ae_sector.rxn2protein.items():
+        if rxn[:2]=='CE': continue
+
+        for enzyme_id, enzyme_dict in enz_dict.items():
+            protein_reaction = enzyme_dict['protein_reaction_association']
+            if not ae_sector._enzyme_is_enzyme_complex(protein_reaction, enzyme_id): continue
+
+            for pr in protein_reaction:
+                if not len(pr) > 1: continue
+
+                enzyme_complex_id = '_'.join(pr)
+                new_rxn2prot[rxn] = {**new_rxn2prot[rxn],
+                                     **{enzyme_complex_id: enzyme_dict}}
+    return new_rxn2prot, protein2gene
+
+def _extract_reaction_id_from_catalytic_reaction_id(input_str: str) -> str:
+    # Define the regex pattern for protein IDs, obtained from UniProtKB, 2024-08-07
+    # https://www.uniprot.org/help/accession_numbers
+    protein_id_pattern = r'(?:[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2})'
+    default_enzyme_id_pattern:str = r'E[0-9][0-9]*'
+
+    # Remove the 'CE_' prefix if it exists
+    if input_str.startswith('CE_'):
+        input_str = input_str[3:]
+
+    # Define the regex pattern to match protein IDs
+    protein_id_regex = re.compile(r'_' + protein_id_pattern + r'|' + r'_' + default_enzyme_id_pattern)
+     # split off all protein ids from the reaction
+    reaction_id = protein_id_regex.split(input_str)[0]
+    # Remove any trailing or leading underscores that might remain
+    return reaction_id.strip('_')
+
+def _get_rxn2kcat_as_series(rxn2kcat: dict[str, dict],
+                                   name: str):
+
+    kcats = {}
+    for rxn,enz_dict in rxn2kcat.items():
+        for enz, kcat_dict in enz_dict.items():
+            for direction, kcat in kcat_dict.items():
+                if len(direction) == 1:
+                    kcats[f"{rxn}_{enz}_{direction}"] = kcat
+    return pd.Series(kcats, name = name)
+
+
+
--- Scripts/restructure_diagnostics_file.py
+++ Scripts/restructure_diagnostics_file.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 
 # Load the enzyme diagnostics file, adjusting column names
-diagnostics_data_path = 'Data/PAM_parametrizer/2025_02_20/pam_parametrizer_diagnostics_mciML1515_1.xlsx'
+diagnostics_data_path = 'Results/PAM_parametrizer/Files/2025_02_20/pam_parametrizer_diagnostics_mciML1515_1.xlsx'
 enzyme_db = pd.read_excel(diagnostics_data_path, sheet_name='Best_Individuals') # this sheet contains the updated kcat of each enzyme for 10 runs
 
 # Group the enzymes after its id and the reaction id and then select only the latest run from each group
@@ -20,8 +20,8 @@ updated_kcat_db['rxn_id'] = updated_kcat_db['rxn_id'].apply(extract_reaction_id)
 
 # Merge updated kcats with mcpam enzymatic data
 ## Load the mcpam enzymatic data
-mcpam_data_path = 'Data/proteinAllocationModel_mciML1515_EnzymaticData_241209_multi.xlsx'
-mcpam_db = pd.read_excel(mcpam_data_path, sheet_name="mcPAM_data_summed_mm")
+mcpam_data_path = 'Results/PAM_parametrizer/Files/2025_02_20/proteinAllocationModel_iML1515_EnzymaticData_multi.xlsx'
+mcpam_db = pd.read_excel(mcpam_data_path, sheet_name="ActiveEnzymes")
 merged_db = pd.merge(mcpam_db, updated_kcat_db, on=['enzyme_id', 'rxn_id', 'direction'], how='left')
 
 for i, row in merged_db.iterrows():
@@ -32,7 +32,7 @@ for i, row in merged_db.iterrows():
 new_db = merged_db.drop(columns=['run_id', 'kcat[s-1]', 'ga_error'])
 
 #Write excel datasheet
-new_db_data_path = 'Data/PAM_parametrizer/2025_02_20/pam_parametrizer_enzymatic_data_mciML1515.xlsx'
+new_db_data_path = 'Results/PAM_parametrizer/Files/2025_02_20/proteinAllocationModel_iML1515_EnzymaticData_multi.xlsx'
 with pd.ExcelWriter(new_db_data_path, engine='openpyxl', mode='a') as writer:
     # Write the new DataFrame to a new sheet
     new_db.to_excel(writer, sheet_name='diagnostics_1', index=True)
