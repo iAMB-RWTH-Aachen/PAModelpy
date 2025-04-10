620c8119bbd4af7fa842c2cd8cdbc09873987ad8
samira.vandenbogaard <samira.vandenbogaard@gmail.com>
2025-04-10 17:41:29 +0200

    added utility to show how much of the protein sectors are occupied at a certain substrate uptake rate

---
+++ src/PAModelpy/utils/visualize_protein_sectors.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+from typing import Iterable
+
+from ..PAModel import PAModel
+
+def run_simulations(pamodel:PAModel,
+                    substrate_rates:Iterable[float],
+                    sub_uptake_id = 'EX_glc__D_e'
+                    ) -> list:
+    fluxes = []
+    total_enzyme_concentration = []
+    for substrate in substrate_rates:
+        if substrate<0:
+            pamodel.change_reaction_bounds(rxn_id=sub_uptake_id,
+                                       lower_bound=substrate, upper_bound=0)
+        else:
+            pamodel.change_reaction_bounds(rxn_id=sub_uptake_id,
+                                       lower_bound=0, upper_bound=substrate)
+
+        print('Running simulations with ', substrate, 'mmol/g_cdw/h of substrate going into the system')
+        sol_pam =pamodel.optimize()
+        if pamodel.solver.status == 'optimal' and pamodel.objective.value>0:
+            fluxes.append(sol_pam.fluxes)
+            total_enzyme_concentration.append(sum([enz.concentration*1e-3*enz.molmass for enz in pamodel.enzyme_variables]))
+    return fluxes, total_enzyme_concentration
+
+def visualize_protein_sectors(pamodel:PAModel,
+                              substrate_rates:Iterable[float],
+                              sub_uptake_id = 'EX_glc__D_e',
+                              ax:plt.Axes = None
+                              ) -> plt.Axes:
+    simulated_fluxes, total_enzyme_concentration = run_simulations(
+        pamodel = pamodel,
+        substrate_rates = substrate_rates,
+        sub_uptake_id = sub_uptake_id
+    )
+    sector_protein_fraction = {'ActiveEnzymeSector': total_enzyme_concentration}
+    for sector in pamodel.sectors:
+        if sector.id in sector_protein_fraction: continue
+        print(sector.id, sector.id_list, sector.slope,sector.intercept)
+        sector_protein_fraction[sector.id] = [
+            fluxes[sector.id_list[0]]*sector.slope*1e-3 +sector.intercept*1e-3 for fluxes in simulated_fluxes
+        ]
+    sector_protein_fraction['total'] = [sum([k[i] for k in sector_protein_fraction.values()]) for i in range(len(substrate_rates))]
+
+    if ax is None:
+        fig, ax = plt.subplot()
+
+    ax.hlines(y=0.258,xmin=0,xmax=max([abs(s) for s in substrate_rates]),
+              label = 'metabolic protein fraction',
+              color = 'black')
+    ax.set_xlabel(sub_uptake_id)
+    ax.set_ylabel(r'protein fraction [$\text{g}_{\text{p}}/\text{g}_{\text{CDW}}$]')
+
+    for sector_label, sector_fractions in sector_protein_fraction.items():
+        ax.plot([abs(s) for s in substrate_rates], sector_fractions, label = sector_label)
+
+    ax.legend()
+
+    return ax
+
+# if __name__ == '__main__':
+#     from Scripts.pam_generation import set_up_ecoli_pam
+#     import numpy as np
+#     pamodel = set_up_ecoli_pam(sensitivity=False)
+#     substrate_rates = np.arange(-11,0,1)
+#     fig, ax = plt.subplots()
+#     visualize_protein_sectors(pamodel,
+#                               substrate_rates,
+#                               sub_uptake_id = 'EX_glc__D_e',
+#                               ax=ax)
+#     fig.savefig('example.png')
+
+
+
+
