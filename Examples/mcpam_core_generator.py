4a1f806dc33cb4c5adf0bb428f563b78c4383575
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2025-11-11 15:38:04 +0100

    tested some kcat set

--- Examples/mcpam_core_generator.py
+++ Examples/mcpam_core_generator.py
@@ -5,6 +5,7 @@ from Scripts.create_pamodel_from_diagnostics_file import (create_pamodel_from_di
                                                           )
 from Scripts.mcpam_toy_generation import build_toy_model
 from src.PAModelpy.utils.pam_generation import set_up_pam, set_up_core_pam
+from Scripts.mcpam_generation_uniprot_id import set_up_ecolicore_mcpam_new_surface_parameter, set_up_ecolicore_pam
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import pandas as pd
 import re
@@ -16,19 +17,22 @@ if __name__ == "__main__":
     ## Build full scale pam and change the enzyme sectors accordingly (based on script from Tobias. A)
 
     # Load iML1515 PAM and mcPAM
-    pam_info_path = 'Data/mcPAM_iML1515_EnzymaticData_250627.xlsx'
+    pam_info_path = 'Data/proteinAllocationModel_mc-core_EnzymaticData_241209_multi.xlsx'
     model_path = 'Models/e_coli_core.json'
 
     core_gem = load_json_model(model_path)
-    pam = set_up_core_pam(pam_info_file=pam_info_path,
+    pam = set_up_core_pam(
+                    pam_info_file=pam_info_path,
                     model=model_path,
                     sensitivity=False,
                     membrane_sector=False
                     )
-    mcpam = set_up_core_pam(pam_info_file=pam_info_path, 
+    mcpam = set_up_core_pam(
+                        pam_info_file=pam_info_path, 
                        model=model_path,
-                       sensitivity=False, 
-                       membrane_sector=True)
+                       sensitivity=False,
+                       membrane_sector=True
+                       )
     pam_mcpam = [pam, mcpam]
     models = [core_gem, pam, mcpam]
 
