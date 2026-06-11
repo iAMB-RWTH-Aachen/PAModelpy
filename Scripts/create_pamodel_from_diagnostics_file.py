778c3f1a7a3f036b565e30e2dafe9e33ec27d237
Titania Sugiarto <titania.sugiarto@rwth-aachen.de>
2026-06-11 09:09:49 +0200

    added parametrizer results from 2026_06_10

--- Scripts/create_pamodel_from_diagnostics_file.py
+++ Scripts/create_pamodel_from_diagnostics_file.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
                        enable_unused_membrane_sector=True
                        )
 
-    mcpam = create_pamodel_from_diagnostics_file(file_path='Results/PAM_parametrizer/Diagnostics_files/2026_06_09/pam_parametrizer_diagnostics_mciML1515_4.xlsx',
+    mcpam = create_pamodel_from_diagnostics_file(file_path='Results/PAM_parametrizer/Diagnostics_files/2026_06_09/pam_parametrizer_diagnostics_mciML1515_6.xlsx',
                                                  model=mcpam,
                                                  sheet_name='Best_Individuals')
     # mcpam.change_sector_parameters(mcpam.unused_enzymes, slope=-0.099063584, intercept=0.111925, lin_rxn_id='BIOMASS_Ec_iML1515_core_75p37M', print_change=True)
